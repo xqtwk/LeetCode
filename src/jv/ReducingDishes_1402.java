3ff5cd2a13ce78d3126e092ae7cbbecec2024079
Richard <xqtwkk@gmail.com>
2023-03-29 23:57:07 +0300

    refactoring

--- src/jv/ReducingDishes_1402.java
+++ src/jv/ReducingDishes_1402.java
@@ -2,27 +2,25 @@ package jv;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class ReducingDishes_1402 {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
-        ArrayList<Integer> startList = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         boolean check = false;
         for (int i = 0; i < satisfaction.length; i++) {
-            startList.add(satisfaction[i]);
+            list.add(satisfaction[i]);
             if (satisfaction[i] > 0) {
                 check = true;
             }
         }
-        if (check == false) {
+        if (!check) {
             return 0;
         }
-        int result = checkSum(startList);
-        ArrayList<Integer> temp = startList;
-        for (int i = 0; i < startList.size(); i++) {
-            temp.remove(0);
-            int sum = checkSum(temp);
+        int result = checkSum(list);
+        for (int i = 0; i < list.size(); i++) {
+            list.remove(0);
+            int sum = checkSum(list);
             if (sum > result) {
                 result = sum;
             } else {
