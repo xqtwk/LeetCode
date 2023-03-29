9a2992d8996e5cdf36caa46eda7eacbc3aa02e34
Richard <xqtwkk@gmail.com>
2023-03-29 15:52:01 +0300

    easy tasks w/o 2 pointer

---
+++ src/jv/SquaresOfASortedArray_977.java
@@ -0,0 +1,16 @@
+package jv;
+
+import java.util.Arrays;
+
+public class SquaresOfASortedArray_977 {
+    public int[] sortedSquares(int[] nums) {
+        int first = nums[0];
+        for (int i =0; i < nums.length;i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        if (first < 0) {
+            Arrays.sort(nums);
+        }
+        return nums;
+    }
+}
