56ecb35b2a187dbe8499c0980b210598336c48c6
Richard <xqtwkk@gmail.com>
2023-03-31 16:36:54 +0300

    medium-level task

---
+++ src/jv/RotateArray_189.java
@@ -0,0 +1,18 @@
+package jv;
+
+public class RotateArray_189 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int[] temp = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[nums.length - k + i];
+        }
+        for (int i = nums.length - 1; i >= k; i--) {
+            nums[i] = nums[i - k];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
