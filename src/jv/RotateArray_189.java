a33ac7b1fe480168e03d564a19b7583864fe2d5c
Richard <xqtwkk@gmail.com>
2023-03-31 16:38:36 +0300

    added 1st try, time limit exceeded on last test(had time complexity o(n*k))

--- src/jv/RotateArray_189.java
+++ src/jv/RotateArray_189.java
@@ -16,3 +16,17 @@ public class RotateArray_189 {
         }
     }
 }
+/*
+public class RotateArray_189 {
+    public void rotate(int[] nums, int k) {
+        while (k > 0) {
+            int temp = nums[nums.length-1];
+            for (int i = nums.length-2; i >=0; i--) {
+                nums[i+1] = nums[i];
+            }
+            nums[0] = temp;
+            k--;
+        }
+    }
+}
+ */
