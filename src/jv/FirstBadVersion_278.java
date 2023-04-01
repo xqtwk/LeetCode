761c8de50d9ae89faa4f408f985c0cca4af420ab
Richard <xqtwkk@gmail.com>
2023-04-01 22:05:16 +0300

    commented class to have no problems in ide hahaha

--- src/jv/FirstBadVersion_278.java
+++ src/jv/FirstBadVersion_278.java
@@ -3,6 +3,7 @@ package jv;
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
+/*
 public class FirstBadVersion_278 {
     public int firstBadVersion(int n) {
         int left = 0;
@@ -19,3 +20,4 @@ public class FirstBadVersion_278 {
         return left;
     }
 }
+*/
