429a1c5225aa512f741b97f13953152a8d708750
Richard <xqtwkk@gmail.com>
2023-04-06 14:44:18 +0300

    medium

--- src/jv/Main.java
+++ src/jv/Main.java
@@ -2,8 +2,8 @@ package jv;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {3,5,3,4};
-        BoatsToSavePeople_881 boatsToSavePeople881 = new BoatsToSavePeople_881();
-        System.out.println(boatsToSavePeople881.numRescueBoats(nums,5));
+        TwoSumIIInputArrayIsSorted_167 twoSumIIInputArrayIsSorted167 = new TwoSumIIInputArrayIsSorted_167();
+        int[] nums = {2, 7, 11, 15};
+        twoSumIIInputArrayIsSorted167.twoSum(nums, 9);
     }
 }
---
+++ src/jv/TwoSumIIInputArrayIsSorted_167.java
@@ -0,0 +1,26 @@
+package jv;
+
+public class TwoSumIIInputArrayIsSorted_167 {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers.length < 2) {
+            return null;
+        }
+
+        int[] result = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                result[0] = left + 1;
+                result[1] = right + 1;
+                return result;
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return null;
+    }
+}
