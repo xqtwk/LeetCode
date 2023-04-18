c8ab2baf42c9837c98e5e2c71089b8157f116919
Richard <xqtwkk@gmail.com>
2023-04-19 00:43:29 +0300

    medium: used Kadane's algorithm

--- src/jv/Main.java
+++ src/jv/Main.java
@@ -2,6 +2,8 @@ package jv;
 
 public class Main {
     public static void main(String[] args) {
-
+        MaximumSubarray_53 maximumSubarray53 = new MaximumSubarray_53();
+        int[] nums = {-2,1};
+        System.out.println(maximumSubarray53.maxSubArray(nums));
     }
 }
