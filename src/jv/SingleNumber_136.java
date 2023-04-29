3b52d1eb521bbc7392e4ee479c89d0827f8c98f5
Richard <xqtwkk@gmail.com>
2023-04-29 17:48:04 +0300

    easy: bits manipulation

---
+++ src/jv/SingleNumber_136.java
@@ -0,0 +1,11 @@
+package jv;
+
+public class SingleNumber_136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
