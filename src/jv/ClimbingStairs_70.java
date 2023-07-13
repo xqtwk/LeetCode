e0f0c44e64688486e090444acd2b08924f43c4bc
xqtwk <xqtwkk@gmail.com>
2023-07-13 20:31:12 +0100

    easy but my brain laggs

---
+++ src/jv/ClimbingStairs_70.java
@@ -0,0 +1,16 @@
+package jv;
+
+public class ClimbingStairs_70 {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
+    }
+}
