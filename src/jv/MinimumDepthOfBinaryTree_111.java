0b729f2ca55300a4151eb5aa6dbdfffe27188e15
xqtwk <xqtwkk@gmail.com>
2023-07-22 20:47:26 +0100

    task's setup

---
+++ src/jv/MinimumDepthOfBinaryTree_111.java
@@ -0,0 +1,26 @@
+package jv;
+
+public class MinimumDepthOfBinaryTree_111 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public int minDepth(TreeNode root) {
+
+    }
+}
