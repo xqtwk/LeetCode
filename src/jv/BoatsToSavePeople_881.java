de318636d279959d6018b2c5a6e336c3c1643234
Richard <xqtwkk@gmail.com>
2024-01-27 18:36:29 +0300

    idk it just was there lol

---
+++ src/jv/BoatsToSavePeople_881.java
@@ -0,0 +1,14 @@
+package jv;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BoatsToSavePeople_881 {
+    /*
+    public int numRescueBoats(int[] people, int limit) {
+        ArrayList<Integer> p = Arrays.asList(people);
+    }
+        for (int i = 0; i < people.length; i++) {
+        }
+    */
+}
