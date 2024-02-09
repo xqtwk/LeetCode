60bfc4dde34eeb2008fa3808e38e4d634be0ad93
Richard <xqtwkk@gmail.com>
2024-02-09 19:45:08 +0300

    failed, DROP for now lol

---
+++ src/jv/LargestDivisibleSubset.java
@@ -0,0 +1,18 @@
+package jv;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LargestDivisibleSubset {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 4, 8};
+        List<Integer> result = largestDivisibleSubset(nums);
+        System.out.println(result);  // Output: [1, 2, 4, 8]
+    }
+
+    public static List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+
+    }
+}
