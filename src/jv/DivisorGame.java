855c7ce3ff1bce24549c39813e33ed915d3ffe67
Richard <xqtwkk@gmail.com>
2024-02-09 19:53:57 +0300

    1line

--- src/jv/DivisorGame.java
+++ src/jv/DivisorGame.java
@@ -2,9 +2,6 @@ package jv;
 
 public class DivisorGame {
     public boolean divisorGame(int n) {
-        if (n % 2 == 0) {
-            return true;
-        }
-        return false;
+        return n % 2 == 0;
     }
 }
