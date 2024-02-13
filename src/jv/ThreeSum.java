0c260950c22025902e56a02781ea7502488b8bce
Richard <xqtwkk@gmail.com>
2024-02-13 19:02:37 +0300

    threesum

---
+++ src/jv/ThreeSum.java
@@ -0,0 +1,27 @@
+package jv;
+
+import java.util.*;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return new ArrayList<>(s);
+    }
+}
