fac6730e827a02de4932daf2624fd04cbf002fa4
Richard <xqtwkk@gmail.com>
2024-02-13 19:07:52 +0300

    moved listnode realisation

--- src/jv/AddTwoNumbers.java
+++ src/jv/AddTwoNumbers.java
@@ -1,11 +1,5 @@
 package jv;
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+
 public class AddTwoNumbers {
     public static void main(String[] args) {
         AddTwoNumbers x = new AddTwoNumbers();
