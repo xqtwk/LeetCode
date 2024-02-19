52f43627b5a60f00d5a03bef99fce4fb03e5feaa
xqtwk <xqtwkk@gmail.com>
2024-02-19 17:34:13 +0200

    medium

--- src/jv/LargestDivisibleSubset.java
+++ src/jv/LargestDivisibleSubset.java
@@ -12,7 +12,39 @@ public class LargestDivisibleSubset {
     }
 
     public static List<Integer> largestDivisibleSubset(int[] nums) {
-        int n = nums.length;
-        return new ArrayList<>();
+        int length = nums.length;
+        if (nums.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(nums);
+
+        int[] dp = new int[length];
+        Arrays.fill(dp, 1);
+        int maxIndex = 0;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            if (dp[maxIndex] < dp[i]) {
+                maxIndex = i;
+            }
+        }
+        int subsetSize = dp[maxIndex];
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = maxIndex; subsetSize > 0; i--) {
+            if (nums[maxIndex] % nums[i] == 0 && dp[i] == subsetSize) {
+                result.add(nums[i]);
+                maxIndex = i;
+                subsetSize--;
+            }
+        }
+        return result;
     }
 }
