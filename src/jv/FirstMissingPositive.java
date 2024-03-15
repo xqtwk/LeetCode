4e2913f037e21b9ca1b0950edd63c1bac069c7ad
Richard <xqtwkk@gmail.com>
2024-03-15 20:21:03 +0300

    stupid hard solution

---
+++ src/jv/FirstMissingPositive.java
@@ -0,0 +1,35 @@
+package jv;
+
+import java.util.Arrays;
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        System.out.println(firstMissingPositive(new int[]{1, 2, 0}));
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        int firstPositiveIndex = 0;
+        Arrays.sort(nums);
+
+        if (nums.length == 0 || nums[nums.length - 1] <= 0) {
+            return 1;
+        }
+
+
+        while (firstPositiveIndex < nums.length && nums[firstPositiveIndex] <= 0) {
+            firstPositiveIndex++;
+        }
+
+        if (firstPositiveIndex == nums.length || nums[firstPositiveIndex] != 1) {
+            return 1;
+        }
+
+        for (int i = firstPositiveIndex; i < nums.length - 1; i++) {
+            if (nums[i] + 1 < nums[i + 1]) {
+                return nums[i] + 1;
+            }
+        }
+
+        return nums[nums.length - 1] + 1;
+    }
+}
