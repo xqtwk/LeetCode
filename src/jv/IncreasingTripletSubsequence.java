b0d96ec498427167822dd4cb8a7ef3a642f65150
Richard <xqtwkk@gmail.com>
2024-04-06 17:16:08 +0300

    md

---
+++ src/jv/IncreasingTripletSubsequence.java
@@ -0,0 +1,14 @@
+package jv;
+
+public class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        int one = Integer.MAX_VALUE;
+        int two = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num <= one) one = num;
+            else if (num <= two) two = num;
+            else return true;
+        }
+        return false;
+    }
+}
