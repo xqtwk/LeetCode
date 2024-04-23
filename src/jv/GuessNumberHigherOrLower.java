c4f93dfa9dd9a2361f1835bae7425668a680e812
xqtwk <xqtwkk@gmail.com>
2024-04-23 17:20:28 +0300

    easy

---
+++ src/jv/GuessNumberHigherOrLower.java
@@ -0,0 +1,19 @@
+package jv;
+
+public class GuessNumberHigherOrLower {
+    /*public int guessNumber(int n) {
+        int left = 1;
+        while (left <= n) {
+            int mid = left + (n - left) / 2;
+            int result = guess(mid);
+            if (guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) == -1) {
+                n = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }*/
+}
